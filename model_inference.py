19e9a1f15d289934441931ec0f9cd738d2741ee7
mykola9208 <117111441+mykola9208@users.noreply.github.com>
2023-06-27 05:22:10 +0300

    test project completed

---
+++ model_inference.py
@@ -0,0 +1,62 @@
+import pandas as pd
+import os
+import numpy as np
+from imageio.v2 import imread
+from skimage.transform import resize
+# щоб не повторювати параметри, грузимо їх з іншого файлу та грузимо модель
+from model_training import model, image_shape, IMG_WIDTH, IMG_HEIGHT, TARGET_WIDTH, TARGET_HEIGHT, data_dir
+
+
+FAST_PREDICTION = True  # використовується для більш швидкої перевірки роботи програми
+FAST_PREDICTION_SIZE = 1000
+sub_df = pd.read_csv(os.path.join(data_dir, 'sample_submission_v2.csv'))
+
+no_mask = np.zeros(image_shape[0]*image_shape[1], dtype=np.uint8)
+
+if FAST_PREDICTION:
+    sub_df = sub_df.sample(n=FAST_PREDICTION_SIZE).reset_index().drop(columns=["index"])  # after reset index dataframe will have one more column call index
+
+model.load_weights('../airbus_test_project/seg_model_weights.best.hdf5')
+
+
+# описуємо функцію енкодера для маски, щоб записати потім у файл
+
+def rle_encode(img):
+    pixels = img.T.flatten()
+    pixels = np.concatenate([[0], pixels, [0]])
+    runs = np.where(pixels[1:] != pixels[:-1])[0] + 1
+    runs[1::2] -= runs[::2]
+    return ' '.join(str(x) for x in runs)
+
+
+def get_test_image(image_name):
+    img = imread(os.path.join(data_dir, 'test_v2/') + image_name)
+    img = resize(img, (TARGET_WIDTH, TARGET_HEIGHT), mode='constant', preserve_range=True)
+    return img
+
+
+def create_test_generator():
+    while True:
+        for k, ix in sub_df.groupby(np.arange(sub_df.shape[0])):
+            imgs = []
+            for indx, rows in ix.iterrows():
+                original_img = get_test_image(rows.ImageId) / 255.0
+                imgs.append(original_img)
+
+            imgs = np.array(imgs)
+            yield imgs
+
+
+test_generator = create_test_generator()
+
+test_steps = np.ceil(float(sub_df.shape[0])).astype(int)
+predict_mask = model.predict(test_generator, steps=test_steps)
+
+for index, row in sub_df.iterrows():
+    predict = predict_mask[index]
+    resized_predict = resize(predict, (IMG_WIDTH, IMG_HEIGHT)) * 255
+    mask = resized_predict > 0.5
+    sub_df.at[index, 'EncodedPixels'] = rle_encode(mask)
+
+# зберегаємо у необхідний файл, але я зберігаю у інший файл, щоб не змінювати кількість рядків у файлі sample_submission_v2.csv
+sub_df.to_csv(os.path.join(data_dir, 'sample_submission_result.csv'), index=False)
---
+++ model_training.py
@@ -0,0 +1,258 @@
+# завантажуемо бібліотеки
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+from keras.optimizers import Adam
+from keras.losses import binary_crossentropy
+import keras.backend as K
+from keras.callbacks import ModelCheckpoint, EarlyStopping, ReduceLROnPlateau
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Input, concatenate, Conv2D, MaxPooling2D, Activation, UpSampling2D, \
+    BatchNormalization
+from skimage.transform import resize
+from sklearn.model_selection import train_test_split
+from imageio.v2 import imread
+import os
+
+# вводимо гіперпараметри
+
+IMG_WIDTH = 768
+IMG_HEIGHT = 768
+TARGET_WIDTH = 128
+TARGET_HEIGHT = 128
+IMG_CHANNELS = 3
+data_dir = '../airbus_test_project/dataset/'  # посилання на папку, в якій знаходиться база данних
+epochs = 5
+image_shape = (768, 768)
+numb_test = 200  # кількість картинок, що буде задіяна для швидкої перевірки
+FAST_RUN = True  # використовується для більш швидкої перевірки роботи програми
+
+df = pd.read_csv(os.path.join(data_dir, 'train_ship_segmentations_update.csv'))
+
+no_mask = np.zeros(image_shape[0] * image_shape[1], dtype=np.uint8)
+
+
+# описуємо функцію для декодування пікселей з судами
+
+
+def rle_decode(mask_rle, shape=image_shape):
+
+    if pd.isnull(mask_rle):
+        img = no_mask
+        return img.reshape(shape).T
+    s = mask_rle.split()
+    starts, lengths = [np.asarray(x, dtype=int) for x in (s[0:][::2], s[1:][::2])]
+
+    starts -= 1
+    ends = starts + lengths
+    img = np.zeros(shape[0] * shape[1], dtype=np.uint8)
+    for lo, hi in zip(starts, ends):
+        img[lo:hi] = 1
+    return img.reshape(shape).T
+
+
+# опсиуэмо модель
+
+
+inputs = Input((TARGET_WIDTH, TARGET_HEIGHT, IMG_CHANNELS))
+
+# 128
+
+down1 = Conv2D(64, (3, 3), padding='same')(inputs)
+down1 = BatchNormalization()(down1)
+down1 = Activation('relu')(down1)
+down1 = Conv2D(64, (3, 3), padding='same')(down1)
+down1 = BatchNormalization()(down1)
+down1 = Activation('relu')(down1)
+down1_pool = MaxPooling2D((2, 2), strides=(2, 2))(down1)
+# 64
+
+down2 = Conv2D(128, (3, 3), padding='same')(down1_pool)
+down2 = BatchNormalization()(down2)
+down2 = Activation('relu')(down2)
+down2 = Conv2D(128, (3, 3), padding='same')(down2)
+down2 = BatchNormalization()(down2)
+down2 = Activation('relu')(down2)
+down2_pool = MaxPooling2D((2, 2), strides=(2, 2))(down2)
+# 32
+
+down3 = Conv2D(256, (3, 3), padding='same')(down2_pool)
+down3 = BatchNormalization()(down3)
+down3 = Activation('relu')(down3)
+down3 = Conv2D(256, (3, 3), padding='same')(down3)
+down3 = BatchNormalization()(down3)
+down3 = Activation('relu')(down3)
+down3_pool = MaxPooling2D((2, 2), strides=(2, 2))(down3)
+# 16
+
+down4 = Conv2D(512, (3, 3), padding='same')(down3_pool)
+down4 = BatchNormalization()(down4)
+down4 = Activation('relu')(down4)
+down4 = Conv2D(512, (3, 3), padding='same')(down4)
+down4 = BatchNormalization()(down4)
+down4 = Activation('relu')(down4)
+down4_pool = MaxPooling2D((2, 2), strides=(2, 2))(down4)
+# 8
+
+center = Conv2D(1024, (3, 3), padding='same')(down4_pool)
+center = BatchNormalization()(center)
+center = Activation('relu')(center)
+center = Conv2D(1024, (3, 3), padding='same')(center)
+center = BatchNormalization()(center)
+center = Activation('relu')(center)
+# center
+
+up4 = UpSampling2D((2, 2))(center)
+up4 = concatenate([down4, up4], axis=3)
+up4 = Conv2D(512, (3, 3), padding='same')(up4)
+up4 = BatchNormalization()(up4)
+up4 = Activation('relu')(up4)
+up4 = Conv2D(512, (3, 3), padding='same')(up4)
+up4 = BatchNormalization()(up4)
+up4 = Activation('relu')(up4)
+up4 = Conv2D(512, (3, 3), padding='same')(up4)
+up4 = BatchNormalization()(up4)
+up4 = Activation('relu')(up4)
+# 16
+
+up3 = UpSampling2D((2, 2))(up4)
+up3 = concatenate([down3, up3], axis=3)
+up3 = Conv2D(256, (3, 3), padding='same')(up3)
+up3 = BatchNormalization()(up3)
+up3 = Activation('relu')(up3)
+up3 = Conv2D(256, (3, 3), padding='same')(up3)
+up3 = BatchNormalization()(up3)
+up3 = Activation('relu')(up3)
+up3 = Conv2D(256, (3, 3), padding='same')(up3)
+up3 = BatchNormalization()(up3)
+up3 = Activation('relu')(up3)
+# 32
+
+up2 = UpSampling2D((2, 2))(up3)
+up2 = concatenate([down2, up2], axis=3)
+up2 = Conv2D(128, (3, 3), padding='same')(up2)
+up2 = BatchNormalization()(up2)
+up2 = Activation('relu')(up2)
+up2 = Conv2D(128, (3, 3), padding='same')(up2)
+up2 = BatchNormalization()(up2)
+up2 = Activation('relu')(up2)
+up2 = Conv2D(128, (3, 3), padding='same')(up2)
+up2 = BatchNormalization()(up2)
+up2 = Activation('relu')(up2)
+# 64
+
+up1 = UpSampling2D((2, 2))(up2)
+up1 = concatenate([down1, up1], axis=3)
+up1 = Conv2D(64, (3, 3), padding='same')(up1)
+up1 = BatchNormalization()(up1)
+up1 = Activation('relu')(up1)
+up1 = Conv2D(64, (3, 3), padding='same')(up1)
+up1 = BatchNormalization()(up1)
+up1 = Activation('relu')(up1)
+up1 = Conv2D(64, (3, 3), padding='same')(up1)
+up1 = BatchNormalization()(up1)
+up1 = Activation('relu')(up1)
+# 128
+
+outputs = Conv2D(1, (1, 1), activation='sigmoid')(up1)
+
+model = Model(inputs=inputs, outputs=outputs)
+
+optimizer = tf.keras.optimizers.RMSprop(0.0001)
+
+model.compile(
+    optimizer=optimizer,
+    loss="binary_crossentropy",
+    metrics=["accuracy"]
+)
+
+if FAST_RUN:
+    df = df.sample(numb_test).reset_index().drop(
+        columns=["index"])  # для швидкої перевірки роботи програми
+
+train_df, validate_df = train_test_split(df, test_size=0.3)
+
+
+def get_image(image_name):
+    img = imread(os.path.join(data_dir, 'train_v2/') + image_name)[:, :, :IMG_CHANNELS]
+    img = resize(img, (TARGET_WIDTH, TARGET_HEIGHT), mode='constant', preserve_range=True)
+    return img
+
+
+def get_mask(code):
+    img = rle_decode(code)
+    img = resize(img, (TARGET_WIDTH, TARGET_HEIGHT, 1), mode='constant', preserve_range=True)
+    return img
+
+
+def create_image_generator(precess_batch_size, data_df):
+    while True:
+        for k, group_df in data_df.groupby(np.arange(data_df.shape[0]) // precess_batch_size):
+            imgs = []
+            labels = []
+            for index, row in group_df.iterrows():
+                # images
+                original_img = get_image(row.ImageId) / 255.0
+                # masks
+                mask = get_mask(row.EncodedPixels) / 255.0
+
+                imgs.append(original_img)
+                labels.append(mask)
+
+            imgs = np.array(imgs)
+            labels = np.array(labels)
+            yield imgs, labels
+
+
+train_generator = create_image_generator(epochs, train_df)
+validate_generator = create_image_generator(epochs, validate_df)
+
+
+# описуємо коефіцієнти
+
+def dice_coef(y_true, y_pred, smooth=1):
+    intersection = K.sum(y_true * y_pred, axis=[1, 2, 3])
+    union = K.sum(y_true, axis=[1, 2, 3]) + K.sum(y_pred, axis=[1, 2, 3])
+    return K.mean((2. * intersection + smooth) / (union + smooth), axis=0)
+
+
+def dice_p_bce(in_gt, in_pred):
+    return 1e-3 * binary_crossentropy(in_gt, in_pred) - dice_coef(in_gt, in_pred)
+
+
+def true_positive_rate(y_true, y_pred):
+    return K.sum(K.flatten(y_true) * K.flatten(K.round(y_pred))) / K.sum(y_true)
+
+# описуємо модель
+
+
+model.compile(optimizer=Adam(1e-4, decay=1e-6), loss=dice_p_bce,
+              metrics=[dice_coef, 'binary_accuracy', true_positive_rate])
+
+weight_path = "{}_weights.best.hdf5".format('seg_model')
+
+checkpoint = ModelCheckpoint(weight_path, monitor='val_dice_coef', verbose=1,
+                             save_best_only=True, mode='max', save_weights_only=True)
+
+reduceLROnPlat = ReduceLROnPlateau(monitor='val_dice_coef', factor=0.5,
+                                   patience=3,
+                                   verbose=1, mode='max', min_delta=0.0001, cooldown=2, min_lr=1e-6)
+early = EarlyStopping(monitor="val_dice_coef",
+                      mode="max",
+                      patience=15)
+callbacks_list = [checkpoint, early, reduceLROnPlat]
+
+
+train_steps = np.ceil(float(train_df.shape[0]) / float(epochs)).astype(int)
+validate_steps = np.ceil(float(validate_df.shape[0]) / float(epochs)).astype(int)
+
+if __name__ == '__main__':
+    history = model.fit(
+        train_generator,
+        steps_per_epoch=train_steps,
+        validation_data=validate_generator,
+        validation_steps=validate_steps,
+        callbacks=callbacks_list,
+        epochs=epochs
+        )
+
